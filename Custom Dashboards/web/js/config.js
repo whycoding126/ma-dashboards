8e279901b8b77fc120cfa8cbf6f39e6b77bac178
Jared Wiltshire <jazdw@users.noreply.github.com>
2015-03-06 12:54:41 +1100

    Enable using the Dojo require loader instead of RequireJS loader.

--- Custom Dashboards/web/js/config.js
+++ Custom Dashboards/web/js/config.js
@@ -1,4 +1,23 @@
-var require = {
+/**
+ * Copyright (C) 2015 Infinite Automation Systems, Inc. All rights reserved.
+ * http://infiniteautomation.com/
+ * @author Jared Wiltshire
+ */
+
+(function(root){
+
+var loader;
+
+var scriptTags = document.getElementsByTagName('script');
+for (var i = 0; i < scriptTags.length; i++) {
+    var script = scriptTags[i];
+    if (script.getAttribute('src') === '/modules/dashboards/web/js/config.js') {
+        loader = script.getAttribute('data-loader') || 'RequireJS';
+        break;
+    }
+}
+
+var requireJsConfig = {
     baseUrl : '/modules/dashboards/web/js',
     paths: {
         'mango': '/mango-javascript/v1',
@@ -65,3 +84,55 @@ var require = {
         }
     }
 };
+
+if (loader === 'RequireJS') {
+    // export require to global scope
+    root.require = requireJsConfig;
+}
+else if (loader === 'Dojo') {
+    var paths = requireJsConfig.paths;
+    
+    var dojoConfig = this.dojoConfig = {
+        baseUrl: requireJsConfig.baseUrl,
+        tlmSiblingOfDojo: false,
+        // load jquery before anything else so we can put it in noConflict mode
+        deps: ['jquery'],
+        callback: function($) {
+            // remove $ from the global scope, jQuery global is still available
+            // $ is defined by DWR and is used in Mango legacy scripts
+            $.noConflict();
+        },
+        packages: [{name: 'dojo', location: '/resources/dojo'},
+                   {name: 'dojox', location: '/resources/dojox'},
+                   {name: 'dijit', location: '/resources/dijit'},
+                   {name: 'dgrid', location: '/resources/dgrid', main: 'OnDemandGrid'},
+                   {name: 'xstyle', location: '/resources/xstyle'},
+                   {name: 'put-selector', location: '/resources/put-selector', main: 'put'}
+                   ],
+        aliases: []
+    };
+    
+    for (var packageName in paths) {
+        var path = paths[packageName];
+        // turn paths ending with .min and names starting with amcharts into
+        // aliases instead of packages
+        if (path.indexOf('.min', path.length - 4) !== -1 ||
+                packageName.slice(0, 8) === 'amcharts') {
+            dojoConfig.aliases.push([
+                packageName,
+                path
+            ]);
+        }
+        else {
+            dojoConfig.packages.push({
+                name: packageName,
+                location: path
+            });
+        }
+    }
+    
+    // export dojoConfig to global scope
+    root.dojoConfig = dojoConfig;
+}
+
+})(this); // execute anonymous function
