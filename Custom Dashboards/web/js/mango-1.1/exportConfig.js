57c23cc5687d5314b26b78b0a814e3b76d854e1b
Jared Wiltshire <jazdw@users.noreply.github.com>
2015-03-26 16:54:57 +1100

    Make export config work, add advanced options back in later

--- Custom Dashboards/web/js/mango-1.1/exportConfig.js
+++ Custom Dashboards/web/js/mango-1.1/exportConfig.js
@@ -8,71 +8,12 @@ define(['amcharts/exporting/amexport', 'amcharts/exporting/canvg', 'amcharts/exp
         'amcharts/exporting/jspdf', 'amcharts/exporting/jspdf.plugin.addimage'], function() {
 
 var amExport = {
-        top     : 0,
+        top         : 0,
         right       : 0,
         exportJPG   : true,
         exportPNG   : true,
         exportSVG   : true,
-
-        // Advanced configuration
-        userCFG: {
-            menuTop     : 'auto',
-            menuLeft    : 'auto',
-            menuRight   : '0px',
-            menuBottom  : '0px',
-            menuItems   : [{
-                textAlign : 'center',
-                icon      : '../amcharts/images/export.png',
-                iconTitle : 'Save chart as an image',
-                onclick   : function () {},
-                items     : [{
-                    title: 'JPG',
-                    format: 'jpg'
-                }, {
-                    title: 'PNG',
-                    format: 'png'
-                }, {
-                    title: 'SVG',
-                    format: 'svg'
-                }, {
-                    title: 'PDF',
-                    format: 'pdf'
-                }]
-            }],
-            menuItemStyle: {
-                backgroundColor     : 'transparent',
-                opacity         : 1,
-                rollOverBackgroundColor : '#EFEFEF',
-                color           : '#000000',
-                rollOverColor       : '#CC0000',
-                paddingTop      : '6px',
-                paddingRight        : '6px',
-                paddingBottom       : '6px',
-                paddingLeft     : '6px',
-                marginTop       : '0px',
-                marginRight     : '0px',
-                marginBottom        : '0px',
-                marginLeft      : '0px',
-                textAlign       : 'left',
-                textDecoration      : 'none',
-                fontFamily      : 'Arial', // Default: charts default
-                fontSize        : '12px', // Default: charts default
-            },
-            menuItemOutput: {
-                backgroundColor     : '#FFFFFF',
-                fileName        : 'amCharts',
-                format          : 'png',
-                output          : 'dataurlnewwindow',
-                render          : 'browser',
-                dpi         : 90,
-                onclick         : function(instance, config, event) {
-                    event.preventDefault();
-                    instance.output(config);
-                }
-            },
-            legendPosition: "bottom", //top,left,right
-            removeImagery: true
-        }
+        exportPFG   : true
 };
 
 var amChart = {
