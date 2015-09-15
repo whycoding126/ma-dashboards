32aaa896e5a5d0907f51adb023a2b299eb9c7ff5
terrypacker <username@users.noreply.github.com>
2015-09-15 13:23:19 -1000

    Fixing up for changes to Url Security

---
+++ Custom Dashboards/src/com/infiniteautomation/dashboards/web/MangoApiDocsUrlMappingDefinition.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (C) 2015 Infinite Automation Software. All rights reserved.
+ * @author Terry Packer
+ */
+package com.infiniteautomation.dashboards.web;
+
+import com.infiniteautomation.dashboards.DashboardsPermissionDefinition;
+import com.infiniteautomation.dashboards.Lifecycle;
+import com.serotonin.m2m2.db.dao.SystemSettingsDao;
+import com.serotonin.m2m2.module.ModuleRegistry;
+import com.serotonin.m2m2.module.UriMappingDefinition;
+import com.serotonin.m2m2.vo.User;
+import com.serotonin.m2m2.vo.permission.Permissions;
+import com.serotonin.m2m2.web.mvc.UrlHandler;
+
+/**
+ * Class to serve up the Api Documentation from /mango-api-docs/
+ * 
+ * @author Terry Packer
+ *
+ */
+public class MangoApiDocsUrlMappingDefinition extends UriMappingDefinition{
+
+	/* (non-Javadoc)
+	 * @see com.serotonin.m2m2.module.UriMappingDefinition#getPermission()
+	 */
+	@Override
+	public Permission getPermission() {
+		return Permission.CUSTOM;
+	}
+    @Override
+    public boolean hasCustomPermission(User user){
+    	return Permissions.hasPermission(user, SystemSettingsDao.getValue(DashboardsPermissionDefinition.PERMISSION));
+    }
+
+	/* (non-Javadoc)
+	 * @see com.serotonin.m2m2.module.UriMappingDefinition#getPath()
+	 */
+	@Override
+	public String getPath() {
+		return "/mango-api-docs/**/*";
+	}
+
+	/* (non-Javadoc)
+	 * @see com.serotonin.m2m2.module.UriMappingDefinition#getHandler()
+	 */
+	@Override
+	public UrlHandler getHandler() {
+		return new DashboardUrlHandler(ModuleRegistry.getModule(Lifecycle.moduleName).getDirectoryPath(), 
+				"/web/private/api-docs/", "/mango-api-docs/");
+	}
+
+	/* (non-Javadoc)
+	 * @see com.serotonin.m2m2.module.UriMappingDefinition#getJspPath()
+	 */
+	@Override
+	public String getJspPath() {
+		return null;
+	}
+
+}
