fead4e321a2fdd58167a387f6ed2778a123e0003
Jared Wiltshire <jazdw@users.noreply.github.com>
2016-03-01 12:20:51 +1100

    Fix bug with translate

--- Custom Dashboards/web/js/mango-3.0/directives/tr.js
+++ Custom Dashboards/web/js/mango-3.0/directives/tr.js
@@ -23,9 +23,10 @@ function maTr(translate) {
             }, function(error) {
             	return $.Deferred().resolve('!!' + $scope.maTr + '!!');
             }).then(function(text) {
-            	// if element is a text node set the text value
-                if ($elem.get(0).nodeType === 3) {
-                    $elem.get(0).nodeValue = text;
+            	var firstChild = $elem.contents().length && $elem.contents().get(0);
+            	// if first child is a text node set the text value
+                if (firstChild && firstChild.nodeType === 3) {
+                	firstChild.nodeValue = text;
                 } else {
                 	// else prepend a text node to its children
                     $elem.prepend(document.createTextNode(text));
