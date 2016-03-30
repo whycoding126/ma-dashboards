c0ae5c68b15896d6bc464dd2b21f8633b49e0640
Jared Wiltshire <jazdw@users.noreply.github.com>
2016-03-31 10:28:05 +1100

    Small changes

--- Custom Dashboards/web/js/loaderConfig.js
+++ Custom Dashboards/web/js/loaderConfig.js
@@ -5,57 +5,57 @@ var module = '/modules/dashboards/web';
 var vendor = module + '/vendor';
 
 require.config({
-	paths: {
-	  'dashboards': module,
-		'angular-route': vendor + '/angular-route/angular-route',
-		'angular-ui-router': vendor + '/angular-ui-router/angular-ui-router',
-		'json3': vendor + '/json3/json3',
-		'oclazyload': vendor + '/oclazyload/ocLazyLoad',
-		'angular-loading-bar': vendor + '/angular-loading-bar/loading-bar',
-		'angular-bootstrap': vendor + '/angular-bootstrap/ui-bootstrap-tpls',
-		'metisMenu': vendor + '/metisMenu/metisMenu',
-		'ace': vendor + '/ace/ace',
-		'angular-ui-ace': vendor + '/angular-ui-ace/ui-ace',
-    'angular-material': vendor + '/angular-material/angular-material',
-    'angular-animate': vendor + '/angular-animate/angular-animate',
-    'angular-messages': vendor + '/angular-messages/angular-messages',
-    'angular-aria': vendor + '/angular-aria/angular-aria'
-	},
-	shim: {
-		'angular-route': {
-	        deps: ['angular']
-	    },
-	    'angular-ui-router': {
-	        deps: ['angular']
-	    },
-	    'oclazyload': {
-	        deps: ['angular-ui-router']
-	    },
-	    'angular-loading-bar': {
-	        deps: ['angular']
-	    },
-	    'angular-bootstrap': {
-	        deps: ['angular', 'bootstrap']
-	    },
-	    'metisMenu': {
-	    	deps: ['jquery']
-	    },
-	    'angular-ui-ace': {
-	        deps: ['angular', 'ace']
-	    },
-      'angular-material': {
-        deps: ['angular', 'angular-animate', 'angular-aria', 'angular-messages']
-      },
-      'angular-animate': {
-        deps: ['angular']
-      },
-      'angular-messages': {
-        deps: ['angular']
-      },
-      'angular-aria': {
-        deps: ['angular']
-      }
-	}
+    paths : {
+        'dashboards' : module,
+        'angular-route' : vendor + '/angular-route/angular-route',
+        'angular-ui-router' : vendor + '/angular-ui-router/angular-ui-router',
+        'json3' : vendor + '/json3/json3',
+        'oclazyload' : vendor + '/oclazyload/ocLazyLoad',
+        'angular-loading-bar' : vendor + '/angular-loading-bar/loading-bar',
+        'angular-bootstrap' : vendor + '/angular-bootstrap/ui-bootstrap-tpls',
+        'metisMenu' : vendor + '/metisMenu/metisMenu',
+        'ace' : vendor + '/ace/ace',
+        'angular-ui-ace' : vendor + '/angular-ui-ace/ui-ace',
+        'angular-material' : vendor + '/angular-material/angular-material',
+        'angular-animate' : vendor + '/angular-animate/angular-animate',
+        'angular-messages' : vendor + '/angular-messages/angular-messages',
+        'angular-aria' : vendor + '/angular-aria/angular-aria'
+    },
+    shim : {
+        'angular-route' : {
+            deps : ['angular']
+        },
+        'angular-ui-router' : {
+            deps : ['angular']
+        },
+        'oclazyload' : {
+            deps : ['angular-ui-router']
+        },
+        'angular-loading-bar' : {
+            deps : ['angular']
+        },
+        'angular-bootstrap' : {
+            deps : ['angular', 'bootstrap']
+        },
+        'metisMenu' : {
+            deps : ['jquery']
+        },
+        'angular-ui-ace' : {
+            deps : ['angular', 'ace']
+        },
+        'angular-material' : {
+            deps : ['angular', 'angular-animate', 'angular-aria', 'angular-messages']
+        },
+        'angular-animate' : {
+            deps : ['angular']
+        },
+        'angular-messages' : {
+            deps : ['angular']
+        },
+        'angular-aria' : {
+            deps : ['angular']
+        }
+    }
 });
 
 })(this);
--- Custom Dashboards/web/mdAdmin/app.js
+++ Custom Dashboards/web/mdAdmin/app.js
@@ -5,17 +5,18 @@
  * @author Jared Wiltshire
  */
 
-require(['angular',
-         'mango-3.0/maDashboards',
-         'mango-3.0/maAppComponents',
-         'angular-ui-router',
-         'oclazyload',
-         'angular-loading-bar',
-         'angular-material'],
-function(angular, maDashboards, maAppComponents) {
-  'use strict';
+require([
+    'angular',
+    'mango-3.0/maDashboards',
+    'mango-3.0/maAppComponents',
+    'angular-ui-router',
+    'oclazyload',
+    'angular-loading-bar',
+    'angular-material'
+], function(angular, maDashboards, maAppComponents) {
+'use strict';
 
-  var mdAdminApp = angular.module('mdAdminApp', [
+var mdAdminApp = angular.module('mdAdminApp', [
     'oc.lazyLoad',
     'ui.router',
     'angular-loading-bar',
@@ -23,229 +24,201 @@ function(angular, maDashboards, maAppComponents) {
     'maAppComponents',
     'ngMaterial',
     'ngMessages'
- ]);
+]);
 
-  mdAdminApp.run(['$rootScope', '$state', '$timeout', '$mdSidenav', function($rootScope, $state, $timeout, $mdSidenav) {
+mdAdminApp.run([
+    '$rootScope',
+    '$state',
+    '$timeout',
+    '$mdSidenav',
+function($rootScope, $state, $timeout, $mdSidenav) {
     $rootScope.Math = Math;
 
     $rootScope.$on("$stateChangeError", function(event, toState, toParams, fromState, fromParams, error) {
-      if (error && (error.status === 401 || error.status === 403)) {
-        event.preventDefault();
-        $state.loginRedirect = toState;
-        $state.go('login');
-      }
+        if (error && (error.status === 401 || error.status === 403)) {
+            event.preventDefault();
+            $state.loginRedirect = toState;
+            $state.go('login');
+        }
     });
-    
+
     $rootScope.closeMenu = function() {
-      $timeout(function() { $mdSidenav('left').close(); });
+        $timeout(function() {
+            $mdSidenav('left').close();
+        });
     }
 
     $rootScope.openMenu = function() {
-      $timeout(function() { $mdSidenav('left').open(); });
+        $timeout(function() {
+            $mdSidenav('left').open();
+        });
     }
-    
-  }]);
 
-  mdAdminApp.config(['$stateProvider', '$urlRouterProvider', '$ocLazyLoadProvider', '$httpProvider',
-                     '$mdIconProvider', '$mdThemingProvider',
-      function ($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, $httpProvider, $mdIconProvider,
-          $mdThemingProvider, $timeout, $rootScope) {
-    
-    //$mdIconProvider.fontSet('fa', 'fa');
-    $mdThemingProvider.theme('default')
-      .primaryPalette("orange")
-      .accentPalette('indigo')
-      .warnPalette('red');
+}]);
+
+mdAdminApp.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    '$ocLazyLoadProvider',
+    '$httpProvider',
+    '$mdThemingProvider',
+function($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, $httpProvider, $mdThemingProvider) {
+
+    $mdThemingProvider.theme('default').primaryPalette("orange").accentPalette('indigo').warnPalette('red');
     $httpProvider.interceptors.push('errorInterceptor');
 
     $ocLazyLoadProvider.config({
-      debug:false,
-      events:true,
+        debug: false,
+        events: true,
     });
 
     $urlRouterProvider.otherwise('/dashboard/home');
 
-    $stateProvider
-    .state('dashboard', {
-      url:'/dashboard',
-      templateUrl: 'views/dashboard/main.html',
-      resolve: {
-        auth: ['$rootScope', 'User', function($rootScope, User) {
-          $rootScope.user = User.current();
-          return $rootScope.user.$promise;
-        }],
-        loadMyDirectives: function($ocLazyLoad) {
-          return $ocLazyLoad.load({
-            name:'mdAdminApp',
-            files: [
-              'directives/sidebar-date-controls/sidebar-date-controls.js'
-            ]
-          });
+    $stateProvider.state('dashboard', {
+        url: '/dashboard',
+        templateUrl: 'views/dashboard/main.html',
+        resolve: {
+            auth: ['$rootScope', 'User', function($rootScope, User) {
+                $rootScope.user = User.current();
+                return $rootScope.user.$promise;
+            }],
+            loadMyDirectives: function($ocLazyLoad) {
+                return $ocLazyLoad.load({
+                    name: 'mdAdminApp',
+                    files: ['directives/sidebar-date-controls/sidebar-date-controls.js']
+                });
+            }
         }
-      }
-    })
-    .state('dashboard.home', {
-      url: '/home',
-      templateUrl: 'views/dashboard/home.html'
-    })
-    .state('login', {
-      templateUrl: 'views/login.html',
-      url: '/login',
-      resolve: {
-        deps: ['$ocLazyLoad', function($ocLazyLoad) {
-          return $ocLazyLoad.load('directives/login/login.js');
-        }]
-      }
-    })
-    .state('dashboard.examples', {
-      'abstract': true,
-      url: '/examples',
-      template: '<ui-view/>',
-      resolve: {
-        loadMyFile: ['$ocLazyLoad', function($ocLazyLoad) {
-          return $ocLazyLoad.load({
-            name: 'ace',
-            files:['../vendor/ace/ace.js']
-          }).then(function() {
-            return $ocLazyLoad.load({
-              name: 'ui-ace',
-              files:['../vendor/angular-ui-ace/ui-ace.js']
-            });
-          }).then(function() {
-            return $ocLazyLoad.load(
-                ['directives/liveEditor/liveEditor.js',
-                 'directives/liveEditor/livePreview.js',
-                 'directives/liveEditor/dualPaneEditor.js',
-                 'styles/examples.css']
-            );
-          });
-        }]
-      }
-    })
-    .state('dashboard.examples.playArea', {
-      templateUrl:'views/examples/playArea.html',
-      url:'/play-area'
-    })
-    .state('dashboard.examples.playAreaBig', {
-      templateUrl:'views/examples/playAreaBig.html',
-      url:'/play-area-big'
-    })
-    .state('dashboard.examples.templating', {
-      templateUrl:'views/examples/templating.html',
-      url:'/templating'
-    })
-    .state('dashboard.examples.angular', {
-      templateUrl:'views/examples/angular.html',
-      url:'/angular'
-    })
-    .state('dashboard.examples.pageTemplate', {
-      templateUrl:'views/examples/pageTemplate.html',
-      url:'/page-template'
-    })
-    .state('dashboard.examples.pointList', {
-      templateUrl:'views/examples/pointList.html',
-      url:'/point-list'
-    })
-    .state('dashboard.examples.liveValues', {
-      templateUrl:'views/examples/liveValues.html',
-      url:'/live-values'
-    })
-    .state('dashboard.examples.filters', {
-      templateUrl:'views/examples/filters.html',
-      url:'/filters'
-    })
-    .state('dashboard.examples.datePresets', {
-      templateUrl:'views/examples/datePresets.html',
-      url:'/date-presets'
-    })
-    .state('dashboard.examples.pointValues', {
-      templateUrl:'views/examples/pointValues.html',
-      url:'/point-values'
-    })
-    .state('dashboard.examples.getPointByXid', {
-      templateUrl:'views/examples/getPointByXid.html',
-      url:'/get-point-by-xid'
-    })
-    .state('dashboard.examples.latestPointValues', {
-      templateUrl:'views/examples/latestPointValues.html',
-      url:'/latest-point-values'
-    })
-    .state('dashboard.examples.watchdog', {
-      templateUrl:'views/examples/watchdog.html',
-      url:'/watchdog'
-    })
-    .state('dashboard.examples.switchImage', {
-      templateUrl:'views/examples/switchImage.html',
-      url:'/switch-image'
-    })
-    .state('dashboard.examples.bars', {
-      templateUrl:'views/examples/bars.html',
-      url:'/bars'
-    })
-    .state('dashboard.examples.gauges', {
-      templateUrl:'views/examples/gauges.html',
-      url:'/gauges'
-    })
-    .state('dashboard.examples.tanks', {
-      templateUrl:'views/examples/tanks.html',
-      url:'/tanks'
-    })
-    .state('dashboard.examples.translation', {
-      templateUrl:'views/examples/translation.html',
-      url:'/translation'
-    })
-    .state('dashboard.examples.lineChart',{
-      templateUrl:'views/examples/lineChart.html',
-      url:'/line-chart'
-    })
-    .state('dashboard.examples.barChart',{
-      templateUrl:'views/examples/barChart.html',
-      url:'/bar-chart'
-    })
-    .state('dashboard.examples.advancedChart',{
-      templateUrl:'views/examples/advancedChart.html',
-      url:'/advanced-chart'
-    })
-    .state('dashboard.examples.stateChart',{
-      templateUrl:'views/examples/stateChart.html',
-      url:'/state-chart'
-    })
-    .state('dashboard.examples.pointArrayLineChart',{
-      templateUrl:'views/examples/pointArrayLineChart.html',
-      url:'/point-array-line-chart'
-    })
-    .state('dashboard.examples.pointArrayTable',{
-      templateUrl:'views/examples/pointArrayTable.html',
-      url:'/point-array-table'
-    })
-    .state('dashboard.examples.getStatistics',{
-      templateUrl:'views/examples/getStatistics.html',
-      url:'/get-statistics'
-    })
-    .state('dashboard.examples.statePieChart',{
-      templateUrl:'views/examples/statePieChart.html',
-      url:'/state-pie-chart'
-    })
-    .state('dashboard.examples.statisticsTable',{
-      templateUrl:'views/examples/statisticsTable.html',
-      url:'/statistics-table'
-    })
-    .state('dashboard.examples.setPoint', {
-      templateUrl:'views/examples/setPoint.html',
-      url:'/set-point'
-    })
-    .state('dashboard.examples.toggle', {
-      templateUrl:'views/examples/toggle.html',
-      url:'/toggle'
-    })
-    .state('dashboard.examples.jsonStore', {
-      templateUrl:'views/examples/jsonStore.html',
-      url:'/json-store'
+    }).state('dashboard.home', {
+        url: '/home',
+        templateUrl: 'views/dashboard/home.html'
+    }).state('login', {
+        templateUrl: 'views/login.html',
+        url: '/login',
+        resolve: {
+            deps: ['$ocLazyLoad', function($ocLazyLoad) {
+                return $ocLazyLoad.load('directives/login/login.js');
+            }]
+        }
+    }).state('dashboard.examples', {
+        'abstract': true,
+        url: '/examples',
+        template: '<ui-view/>',
+        resolve: {
+            loadMyFile: ['$ocLazyLoad', function($ocLazyLoad) {
+                return $ocLazyLoad.load({
+                    name: 'ace',
+                    files: ['../vendor/ace/ace.js']
+                }).then(function() {
+                    return $ocLazyLoad.load({
+                        name: 'ui-ace',
+                        files: ['../vendor/angular-ui-ace/ui-ace.js']
+                    });
+                }).then(function() {
+                    return $ocLazyLoad.load([
+                        'directives/liveEditor/liveEditor.js',
+                        'directives/liveEditor/livePreview.js',
+                        'directives/liveEditor/dualPaneEditor.js',
+                        'styles/examples.css'
+                    ]);
+                });
+            }]
+        }
+    }).state('dashboard.examples.playArea', {
+        templateUrl: 'views/examples/playArea.html',
+        url: '/play-area'
+    }).state('dashboard.examples.playAreaBig', {
+        templateUrl: 'views/examples/playAreaBig.html',
+        url: '/play-area-big'
+    }).state('dashboard.examples.templating', {
+        templateUrl: 'views/examples/templating.html',
+        url: '/templating'
+    }).state('dashboard.examples.angular', {
+        templateUrl: 'views/examples/angular.html',
+        url: '/angular'
+    }).state('dashboard.examples.pageTemplate', {
+        templateUrl: 'views/examples/pageTemplate.html',
+        url: '/page-template'
+    }).state('dashboard.examples.pointList', {
+        templateUrl: 'views/examples/pointList.html',
+        url: '/point-list'
+    }).state('dashboard.examples.liveValues', {
+        templateUrl: 'views/examples/liveValues.html',
+        url: '/live-values'
+    }).state('dashboard.examples.filters', {
+        templateUrl: 'views/examples/filters.html',
+        url: '/filters'
+    }).state('dashboard.examples.datePresets', {
+        templateUrl: 'views/examples/datePresets.html',
+        url: '/date-presets'
+    }).state('dashboard.examples.pointValues', {
+        templateUrl: 'views/examples/pointValues.html',
+        url: '/point-values'
+    }).state('dashboard.examples.getPointByXid', {
+        templateUrl: 'views/examples/getPointByXid.html',
+        url: '/get-point-by-xid'
+    }).state('dashboard.examples.latestPointValues', {
+        templateUrl: 'views/examples/latestPointValues.html',
+        url: '/latest-point-values'
+    }).state('dashboard.examples.watchdog', {
+        templateUrl: 'views/examples/watchdog.html',
+        url: '/watchdog'
+    }).state('dashboard.examples.switchImage', {
+        templateUrl: 'views/examples/switchImage.html',
+        url: '/switch-image'
+    }).state('dashboard.examples.bars', {
+        templateUrl: 'views/examples/bars.html',
+        url: '/bars'
+    }).state('dashboard.examples.gauges', {
+        templateUrl: 'views/examples/gauges.html',
+        url: '/gauges'
+    }).state('dashboard.examples.tanks', {
+        templateUrl: 'views/examples/tanks.html',
+        url: '/tanks'
+    }).state('dashboard.examples.translation', {
+        templateUrl: 'views/examples/translation.html',
+        url: '/translation'
+    }).state('dashboard.examples.lineChart', {
+        templateUrl: 'views/examples/lineChart.html',
+        url: '/line-chart'
+    }).state('dashboard.examples.barChart', {
+        templateUrl: 'views/examples/barChart.html',
+        url: '/bar-chart'
+    }).state('dashboard.examples.advancedChart', {
+        templateUrl: 'views/examples/advancedChart.html',
+        url: '/advanced-chart'
+    }).state('dashboard.examples.stateChart', {
+        templateUrl: 'views/examples/stateChart.html',
+        url: '/state-chart'
+    }).state('dashboard.examples.pointArrayLineChart', {
+        templateUrl: 'views/examples/pointArrayLineChart.html',
+        url: '/point-array-line-chart'
+    }).state('dashboard.examples.pointArrayTable', {
+        templateUrl: 'views/examples/pointArrayTable.html',
+        url: '/point-array-table'
+    }).state('dashboard.examples.getStatistics', {
+        templateUrl: 'views/examples/getStatistics.html',
+        url: '/get-statistics'
+    }).state('dashboard.examples.statePieChart', {
+        templateUrl: 'views/examples/statePieChart.html',
+        url: '/state-pie-chart'
+    }).state('dashboard.examples.statisticsTable', {
+        templateUrl: 'views/examples/statisticsTable.html',
+        url: '/statistics-table'
+    }).state('dashboard.examples.setPoint', {
+        templateUrl: 'views/examples/setPoint.html',
+        url: '/set-point'
+    }).state('dashboard.examples.toggle', {
+        templateUrl: 'views/examples/toggle.html',
+        url: '/toggle'
+    }).state('dashboard.examples.jsonStore', {
+        templateUrl: 'views/examples/jsonStore.html',
+        url: '/json-store'
     });
-  }]);
+}]);
 
-  angular.element(document).ready(function() {
+angular.element(document).ready(function() {
     angular.bootstrap(document.documentElement, ['mdAdminApp']);
-  });
+});
 
 }); // require
