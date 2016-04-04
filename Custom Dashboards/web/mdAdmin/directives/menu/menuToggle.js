8c2cf5ce49e19740df26112656a0fd67e8c80baf
Jared Wiltshire <jazdw@users.noreply.github.com>
2016-04-04 18:03:50 +1000

    Animate mdAdmin menus

--- Custom Dashboards/web/mdAdmin/directives/menu/menuToggle.js
+++ Custom Dashboards/web/mdAdmin/directives/menu/menuToggle.js
@@ -1,14 +1,18 @@
 'use strict';
 
-angular.module('mdAdminApp').directive('menuToggle', ['$state', function($state) {
+angular.module('mdAdminApp').directive('menuToggle', ['$state', '$timeout', '$rootScope', function($state, $timeout, $rootScope) {
     return {
         scope: {
             section: '='
         },
         templateUrl: 'directives/menu/menuToggle.html',
         link: function($scope, $element) {
+            var $ul = $element.find('ul');
+            
             $scope.open = function() {
                 $scope.isOpen = true;
+                setHeight();
+                $rootScope.$broadcast('menuOpened', $scope.section);
             }
             
             $scope.close = function() {
@@ -24,9 +28,18 @@ angular.module('mdAdminApp').directive('menuToggle', ['$state', function($state)
             };
             
             if ($state.includes($scope.section.state) && !$scope.isOpen) {
-                $scope.open();
+                // use timeout to run open() after ul has been populated by ng-repeat
+                $timeout(function() {
+                    $scope.open();
+                }, 0);
             }
             
+            $scope.$on('menuOpened', function(event, section) {
+                if (!$scope.isOpen || section === $scope.section) return;
+                $scope.close();
+            });
+
+            // close/open menus when changing states
             $scope.$on("$stateChangeSuccess", function(event, toState, toParams, fromState, fromParams) {
                 if ($state.includes($scope.section.state)) {
                     if (!$scope.isOpen) {
@@ -36,33 +49,27 @@ angular.module('mdAdminApp').directive('menuToggle', ['$state', function($state)
                     $scope.close();
                 }
             });
-
-            /*
-            $mdUtil.nextTick(function() {
-                $scope.$watch(function() {
-                    return controller.isOpen($scope.section);
-                }, function(open) {
-                    var $ul = $element.find('ul');
-
-                    var targetHeight = open ? getTargetHeight() : 0;
-                    $timeout(function() {
-                        $ul.css({
-                            height: targetHeight + 'px'
-                        });
-                    }, 0, false);
-
-                    function getTargetHeight() {
-                        var targetHeight;
-                        $ul.addClass('no-transition');
-                        $ul.css('height', '');
-                        targetHeight = $ul.prop('clientHeight');
-                        $ul.css('height', 0);
-                        $ul.removeClass('no-transition');
-                        return targetHeight;
-                    }
+            
+            // calculates the height of ul and sets its height style so transition works correctly.
+            // absolute positioning means it is taken out of usual flow so doesn't affect surrounding
+            // elements while calculating height, however briefly
+            function setHeight() {
+                $ul.addClass('no-transition');
+                $ul.css({
+                    height: '',
+                    visibility: 'hidden',
+                    position: 'absolute'
                 });
-            });
-            */
+                $ul.removeClass('ng-hide');
+                var height = $ul.prop('clientHeight');
+                $ul.addClass('ng-hide');
+                $ul.css({
+                    height: height + 'px',
+                    visibility: '',
+                    position: ''
+                });
+                $ul.removeClass('no-transition');
+            };
         }
     };
 }]);
