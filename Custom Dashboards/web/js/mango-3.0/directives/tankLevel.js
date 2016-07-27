d15cb70beb939e5818aca6e993e22f94745137d4
Jared Wiltshire <jazdw@users.noreply.github.com>
2016-07-27 12:34:54 -0600

    Fix previous commit

--- Custom Dashboards/web/js/mango-3.0/directives/tankLevel.js
+++ Custom Dashboards/web/js/mango-3.0/directives/tankLevel.js
@@ -56,7 +56,7 @@ function tankLevel(maDashboardsInsertCss, cssInjector) {
                 
                 $scope.$watch('point.value', function(newValue, oldValue) {
                     // if gauge already has value set and newValue is undefined just ignore
-                    if (newValue === undefined && chart.arrows[0].value) return;
+                    if (newValue === undefined) return;
                     
                     var rendered;
                     if ($scope.point && typeof $scope.point.renderedValue === 'string') {
