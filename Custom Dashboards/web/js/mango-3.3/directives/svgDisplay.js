d4ca887faaab43a0a0282fdfaa41b828a3124e65
Will Geller <will.geller@gmail.com>
2016-08-29 17:31:47 -0600

    Reads attributes to update shapes with id

--- Custom Dashboards/web/js/mango-3.3/directives/svgDisplay.js
+++ Custom Dashboards/web/js/mango-3.3/directives/svgDisplay.js
@@ -6,34 +6,71 @@
 
 
 define([], function() {
-'use strict';
-/**
- * @ngdoc directive
- * @name maDashboards.maSvgDisplay
- *
- * @description
- * `<ma-svg-display></ma-svg-display>`
- * - This directive will 
- *
- *
- * @param {number} radius Radius of the circle
- *
- * @usage
- * <ma-svg-circle></ma-svg-circle>
- */
-function svgDisplay() {
-    return {
-        restrict: 'E',
-        replace: true,
-        scope: {
-            svg: '@'
-        },
-        template: '<div ng-include="svg"></div>'
-    };
-}
+    'use strict';
+    /**
+     * @ngdoc directive
+     * @name maDashboards.maSvgDisplay
+     *
+     * @description
+     * `<ma-svg-display></ma-svg-display>`
+     * - This directive will display a SVG graphic you can bind data too
+     *
+     *
+     * @param {string} svg Path to the SVG file
+     *
+     * @usage
+     * <ma-svg-display></ma-svg-display>
+     */
+    function svgDisplay() {
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: {
+                svg: '@',
+                point: '=',
+                max: '='
+            },
+            template: '<div ng-include="svg" onload="init()"></div>',
+            link: function($scope, $element, attributes) {
+                $scope.init = function() {
+                    $scope.$watch('point.value', function(newValue, oldValue) {
+                        if (newValue === undefined || newValue === oldValue) return;
+                        update();
+                    });
+                    $scope.$watch('max', function(newValue, oldValue) {
+                        if (newValue === undefined || newValue === oldValue) return;
+                        update();
+                    });
 
-svgDisplay.$inject = [];
+                    var update = function() {
+                        var attrArray = [];
+                        
+                        angular.forEach(attributes, function(value, key) {
+                            attrArray.push([key, value])
+                        });
+                        
+                        attrArray = attrArray.filter(function(item, index, array) {
+                            return item[0].indexOf('shape') !== -1;
+                        });
+                        
+                        attrArray.forEach(function(item, index, array) {
+                            var shapeId = item[0].substring(0, item[0].indexOf('Mode'));
+                            
+                            if (item[1] === 'opacity') {
+                                angular.element(document).find('#' + shapeId).css('fill-opacity', $scope.point.value / $scope.max);
+                            }
+                            else if (item[1] === 'stroke') {
+                                angular.element(document).find('#' + shapeId).css('stroke-width', $scope.point.value / $scope.max * 5);
+                            }
+                        });
+                    }
+                }
+            }
+        };
+    }
 
-return svgDisplay;
+    svgDisplay.$inject = [];
+
+    return svgDisplay;
 
 }); // define
