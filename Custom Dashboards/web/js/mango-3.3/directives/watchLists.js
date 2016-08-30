23460bb6bac42fc1e37bf41e07ec3de5c4bc3679
Jared Wiltshire <jazdw@users.noreply.github.com>
2016-08-30 14:34:05 -0600

    Allow params in custom menu item urls and add select first option to watchlist select

--- Custom Dashboards/web/js/mango-3.3/directives/watchLists.js
+++ Custom Dashboards/web/js/mango-3.3/directives/watchLists.js
@@ -21,34 +21,44 @@ function watchLists(WatchList, $stateParams, $injector, $state) {
             points: '=?',
             watchList: '=?',
             watchListXid: '@',
-            alwaysShowSelect: '=?'
+            alwaysShowSelect: '=?',
+            selectFirst: '=?'
         },
         controller: ['$scope', '$element', '$attrs', function ($scope, $element, $attrs) {
             var xid = $stateParams.watchListXid || this.watchListXid;
             if (xid) {
                 this.watchList = WatchList.get({xid: xid});
                 this.watchList.$promise.then(function(watchList) {
-                    this.points = watchList.points;
+                    this.setWatchList(watchList);
                 }.bind(this));
             }
             if (!xid || this.alwaysShowSelect) {
                 this.showSelect = true;
                 this.queryPromise = WatchList.query().then(function(watchLists) {
                     this.watchLists = watchLists;
+                    
+                    if (!this.watchList && (angular.isUndefined(this.selectFirst) || this.selectFirst) && watchLists.length) {
+                        this.setWatchList(watchLists[0]);
+                    }
+                    
                     return watchLists;
                 }.bind(this));
             }
             
             this.onChange = function() {
-                if (this.watchList) {
-                    this.points = this.watchList.points;
-                    $state.go('.', {watchListXid: this.watchList.xid}, {location: 'replace', notify: false});
-                }
+                setWatchList(this.watchList);
             };
             
             this.onOpen = function() {
                 return this.queryPromise;
             }
+            
+            this.setWatchList = function(watchList) {
+                if (!watchList) return;
+                this.watchList = watchList;
+                this.points = watchList.points;
+                $state.go('.', {watchListXid: this.watchList.xid}, {location: 'replace', notify: false});
+            };
         }]
     };
 }
