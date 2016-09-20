20732d7932367091a5d92906882178c203b92d84
Jared Wiltshire <jazdw@users.noreply.github.com>
2016-09-19 18:41:48 -0600

    Better date pickers in date picker bar

--- Custom Dashboards/web/js/mango-3.3/maDashboards.js
+++ Custom Dashboards/web/js/mango-3.3/maDashboards.js
@@ -128,13 +128,93 @@ maDashboards.component('maPointHierarchyFolder', pointHierarchyFolder);
 maDashboards.filter('tr', trFilter);
 
 maDashboards.constant('maDashboardsInsertCss', true);
-
+maDashboards.constant('MA_DATE_RANGE_PRESETS', [
+   {type: "LAST_5_MINUTES", label: 'Last 5 minutes'},
+   {type: "LAST_15_MINUTES", label: 'Last 15 minutes'},
+   {type: "LAST_30_MINUTES", label: 'Last 30 minutes'},
+   {type: "LAST_1_HOURS", label: 'Last 1 hours'},
+   {type: "LAST_3_HOURS", label: 'Last 3 hours'},
+   {type: "LAST_6_HOURS", label: 'Last 6 hours'},
+   {type: "LAST_12_HOURS", label: 'Last 12 hours'},
+   {type: "LAST_1_DAYS", label: 'Last 1 days'},
+   {type: "LAST_1_WEEKS", label: 'Last 1 weeks'},
+   {type: "LAST_2_WEEKS", label: 'Last 2 weeks'},
+   {type: "LAST_1_MONTHS", label: 'Last 1 months'},
+   {type: "LAST_3_MONTHS", label: 'Last 3 months'},
+   {type: "LAST_6_MONTHS", label: 'Last 6 months'},
+   {type: "LAST_1_YEARS", label: 'Last 1 years'},
+   {type: "LAST_2_YEARS", label: 'Last 2 years'},
+   {type: "DAY_SO_FAR", label: 'Today so far'},
+   {type: "WEEK_SO_FAR", label: 'This week so far'},
+   {type: "MONTH_SO_FAR", label: 'This month so far'},
+   {type: "YEAR_SO_FAR", label: 'This year so far'},
+   {type: "PREVIOUS_DAY", label: 'Yesterday'},
+   {type: "PREVIOUS_WEEK", label: 'Previous week'},
+   {type: "PREVIOUS_MONTH", label: 'Previous month'},
+   {type: "PREVIOUS_YEAR", label: 'Previous year'}
+]);
+maDashboards.constant('MA_ROLLUP_TYPES', [
+ {type: 'NONE', nonNumeric: true, label: 'None'},
+ {type: 'AVERAGE', nonNumeric: false, label: 'Average'},
+ {type: 'DELTA', nonNumeric: false, label: 'Delta'},
+ {type: 'MINIMUM', nonNumeric: false, label: 'Minimum'},
+ {type: 'MAXIMUM', nonNumeric: false, label: 'Maximum'},
+ {type: 'ACCUMULATOR', nonNumeric: false, label: 'Accumulator'},
+ {type: 'SUM', nonNumeric: false, label: 'Sum'},
+ {type: 'FIRST', nonNumeric: true, label: 'First'},
+ {type: 'LAST', nonNumeric: true, label: 'Last'},
+ {type: 'COUNT', nonNumeric: true, label: 'Count'},
+ {type: 'INTEGRAL', nonNumeric: false, label: 'Integral'}
+ //{name: 'FFT', nonNumeric: false}
+]);
+maDashboards.constant('MA_TIME_PERIOD_TYPES', [
+ {type: 'SECONDS', label: 'Seconds'},
+ {type: 'MINUTES', label: 'Minutes'},
+ {type: 'HOURS', label: 'Hours'},
+ {type: 'DAYS', label: 'Days'},
+ {type: 'WEEKS', label: 'Weeks'},
+ {type: 'MONTHS', label: 'Months'},
+ {type: 'YEARS', label: 'Years'}
+]);
+maDashboards.constant('MA_CHART_TYPES', [
+ {type: 'line', label: 'Line'},
+ {type: 'smoothedLine', label: 'Smoothed'},
+ {type: 'step', label: 'Step'},
+ {type: 'column', label: 'Bar'}
+]);
+maDashboards.constant('MA_RELATIVE_DATE_TYPES', [
+ {type: "", label: 'Now'},
+ {type: "moment:'subtract':5:'minutes'", label: '5 minutes ago'},
+ {type: "moment:'subtract':15:'minutes'", label: '15 minutes ago'},
+ {type: "moment:'subtract':30:'minutes'", label: '30 minutes ago'},
+ {type: "moment:'subtract':1:'hours'", label: '1 hour ago'},
+ {type: "moment:'subtract':3:'hours'", label: '3 hours ago'},
+ {type: "moment:'subtract':5:'hours'", label: '6 hours ago'},
+ {type: "moment:'subtract':12:'hours'", label: '12 hours ago'},
+ {type: "moment:'startOf':'day'", label: 'Start of day'},
+ {type: "moment:'subtract':1:'days'|moment:'startOf':'day'", label: 'Start of previous day'},
+ {type: "moment:'subtract':1:'days'", label: '1 day ago'},
+ {type: "moment:'startOf':'week'", label: 'Start of week'},
+ {type: "moment:'subtract':1:'weeks'|moment:'startOf':'week'", label: 'Start of last week'},
+ {type: "moment:'subtract':1:'weeks'", label: '1 week ago'},
+ {type: "moment:'subtract':2:'weeks'", label: '2 weeks ago'},
+ {type: "moment:'startOf':'month'", label: 'Start of month'},
+ {type: "moment:'subtract':1:'months'|moment:'startOf':'month'", label: 'Start of last month'},
+ {type: "moment:'subtract':1:'months'", label: '1 month ago'},
+ {type: "moment:'subtract':3:'months'", label: '3 months ago'},
+ {type: "moment:'subtract':6:'months'", label: '6 months ago'},
+ {type: "moment:'startOf':'year'", label: 'Start of year'},
+ {type: "moment:'subtract':1:'years'|moment:'startOf':'year'", label: 'Start of last year'},
+ {type: "moment:'subtract':1:'years'", label: '1 year ago'}
+]);
 maDashboards.config(['$httpProvider', function($httpProvider) {
 	$httpProvider.interceptors.push('mangoHttpInterceptor');
 }]);
 
 maDashboards.run(['$rootScope', 'mangoWatchdog', 'maDashboardsInsertCss', 'cssInjector',
-                  function($rootScope, mangoWatchdog, maDashboardsInsertCss, cssInjector) {
+                  'MA_ROLLUP_TYPES', 'MA_TIME_PERIOD_TYPES', 'MA_CHART_TYPES', 'MA_RELATIVE_DATE_TYPES', 'MA_DATE_RANGE_PRESETS',
+function($rootScope, mangoWatchdog, maDashboardsInsertCss, cssInjector,
+          MA_ROLLUP_TYPES, MA_TIME_PERIOD_TYPES, MA_CHART_TYPES, MA_RELATIVE_DATE_TYPES, MA_DATE_RANGE_PRESETS) {
 	$rootScope.Math = Math;
     $rootScope.mangoWatchdog = mangoWatchdog;
 
@@ -149,89 +229,11 @@ maDashboards.run(['$rootScope', 'mangoWatchdog', 'maDashboardsInsertCss', 'cssIn
 		return result;
 	};
 
-    $rootScope.rollupTypes = [
-        {type: 'NONE', nonNumeric: true, label: 'None'},
-        {type: 'AVERAGE', nonNumeric: false, label: 'Average'},
-        {type: 'DELTA', nonNumeric: false, label: 'Delta'},
-        {type: 'MINIMUM', nonNumeric: false, label: 'Minimum'},
-        {type: 'MAXIMUM', nonNumeric: false, label: 'Maximum'},
-        {type: 'ACCUMULATOR', nonNumeric: false, label: 'Accumulator'},
-        {type: 'SUM', nonNumeric: false, label: 'Sum'},
-        {type: 'FIRST', nonNumeric: true, label: 'First'},
-        {type: 'LAST', nonNumeric: true, label: 'Last'},
-        {type: 'COUNT', nonNumeric: true, label: 'Count'},
-        {type: 'INTEGRAL', nonNumeric: false, label: 'Integral'}
-        //{name: 'FFT', nonNumeric: false}
-    ];
-
-    $rootScope.timePeriodTypes = [
-        {type: 'SECONDS', label: 'Seconds'},
-        {type: 'MINUTES', label: 'Minutes'},
-        {type: 'HOURS', label: 'Hours'},
-        {type: 'DAYS', label: 'Days'},
-        {type: 'WEEKS', label: 'Weeks'},
-        {type: 'MONTHS', label: 'Months'},
-        {type: 'YEARS', label: 'Years'}
-    ];
-
-    $rootScope.chartTypes = [
-        {type: 'line', label: 'Line'},
-        {type: 'smoothedLine', label: 'Smoothed'},
-        {type: 'step', label: 'Step'},
-        {type: 'column', label: 'Bar'}
-    ];
-
-    $rootScope.relativeDateTypes = [
-        {type: "", label: 'Now'},
-        {type: "moment:'subtract':5:'minutes'", label: '5 minutes ago'},
-        {type: "moment:'subtract':15:'minutes'", label: '15 minutes ago'},
-        {type: "moment:'subtract':30:'minutes'", label: '30 minutes ago'},
-        {type: "moment:'subtract':1:'hours'", label: '1 hour ago'},
-        {type: "moment:'subtract':3:'hours'", label: '3 hours ago'},
-        {type: "moment:'subtract':5:'hours'", label: '6 hours ago'},
-        {type: "moment:'subtract':12:'hours'", label: '12 hours ago'},
-        {type: "moment:'startOf':'day'", label: 'Start of day'},
-        {type: "moment:'subtract':1:'days'|moment:'startOf':'day'", label: 'Start of previous day'},
-        {type: "moment:'subtract':1:'days'", label: '1 day ago'},
-        {type: "moment:'startOf':'week'", label: 'Start of week'},
-        {type: "moment:'subtract':1:'weeks'|moment:'startOf':'week'", label: 'Start of last week'},
-        {type: "moment:'subtract':1:'weeks'", label: '1 week ago'},
-        {type: "moment:'subtract':2:'weeks'", label: '2 weeks ago'},
-        {type: "moment:'startOf':'month'", label: 'Start of month'},
-        {type: "moment:'subtract':1:'months'|moment:'startOf':'month'", label: 'Start of last month'},
-        {type: "moment:'subtract':1:'months'", label: '1 month ago'},
-        {type: "moment:'subtract':3:'months'", label: '3 months ago'},
-        {type: "moment:'subtract':6:'months'", label: '6 months ago'},
-        {type: "moment:'startOf':'year'", label: 'Start of year'},
-        {type: "moment:'subtract':1:'years'|moment:'startOf':'year'", label: 'Start of last year'},
-        {type: "moment:'subtract':1:'years'", label: '1 year ago'}
-    ];
-
-    $rootScope.dateRangePresets = [
-        {type: "LAST_5_MINUTES", label: 'Last 5 minutes'},
-        {type: "LAST_15_MINUTES", label: 'Last 15 minutes'},
-        {type: "LAST_30_MINUTES", label: 'Last 30 minutes'},
-        {type: "LAST_1_HOURS", label: 'Last 1 hours'},
-        {type: "LAST_3_HOURS", label: 'Last 3 hours'},
-        {type: "LAST_6_HOURS", label: 'Last 6 hours'},
-        {type: "LAST_12_HOURS", label: 'Last 12 hours'},
-        {type: "LAST_1_DAYS", label: 'Last 1 days'},
-        {type: "LAST_1_WEEKS", label: 'Last 1 weeks'},
-        {type: "LAST_2_WEEKS", label: 'Last 2 weeks'},
-        {type: "LAST_1_MONTHS", label: 'Last 1 months'},
-        {type: "LAST_3_MONTHS", label: 'Last 3 months'},
-        {type: "LAST_6_MONTHS", label: 'Last 6 months'},
-        {type: "LAST_1_YEARS", label: 'Last 1 years'},
-        {type: "LAST_2_YEARS", label: 'Last 2 years'},
-        {type: "DAY_SO_FAR", label: 'Today so far'},
-        {type: "WEEK_SO_FAR", label: 'This week so far'},
-        {type: "MONTH_SO_FAR", label: 'This month so far'},
-        {type: "YEAR_SO_FAR", label: 'This year so far'},
-        {type: "PREVIOUS_DAY", label: 'Yesterday'},
-        {type: "PREVIOUS_WEEK", label: 'Previous week'},
-        {type: "PREVIOUS_MONTH", label: 'Previous month'},
-        {type: "PREVIOUS_YEAR", label: 'Previous year'}
-    ];
+    $rootScope.rollupTypes = MA_ROLLUP_TYPES;
+    $rootScope.timePeriodTypes = MA_TIME_PERIOD_TYPES;
+    $rootScope.chartTypes = MA_CHART_TYPES;
+    $rootScope.relativeDateTypes = MA_RELATIVE_DATE_TYPES;
+    $rootScope.dateRangePresets = MA_DATE_RANGE_PRESETS;
 }]);
 
 return maDashboards;
