c579666171c4f9f4f73bf0564bcad8357c0141bf
Jared Wiltshire <jazdw@users.noreply.github.com>
2016-10-28 16:29:11 -0600

    Clean up tests

--- Custom Dashboards/web/js/mango-3.3/services/Point.spec.js
+++ Custom Dashboards/web/js/mango-3.3/services/Point.spec.js
@@ -75,8 +75,7 @@ describe('Point service', function() {
     });
 
     it('Get point via XID', runDigestAfter(function() {
-        var promise = Point.get({xid: 'voltage'}).$promise
-        .then(function(point) {
+        return Point.get({xid: 'voltage'}).$promise.then(function(point) {
             checkPoint(point);
             assert.equal(point.xid, 'voltage');
             assert.equal(point.name, 'Voltage');
@@ -86,12 +85,10 @@ describe('Point service', function() {
         }, function(error) {
             throw new Error(error.status + ' - ' + error.statusText);
         });
-        return promise;
     }));
     
     it('Get point via ID', runDigestAfter(function() {
-        var promise = Point.getById({id: voltagePointId}).$promise
-        .then(function(point) {
+        return Point.getById({id: voltagePointId}).$promise.then(function(point) {
             checkPoint(point);
             assert.equal(point.xid, 'voltage');
             assert.equal(point.id, voltagePointId);
@@ -101,24 +98,20 @@ describe('Point service', function() {
         }, function(error) {
             throw new Error(error.status + ' - ' + error.statusText);
         });
-        return promise;
     }));
 
     it('Get non-existing point via XID', runDigestAfter(function() {
-        var promise = Point.get({xid: '003a5f46-b239-4bf4-9a8a-d71643f282db'}).$promise
-        .then(function() {
+        return Point.get({xid: '003a5f46-b239-4bf4-9a8a-d71643f282db'}).$promise.then(function() {
             throw new Error('Shouldn\'t get a point for a random XID');
         }, function(response) {
             assert.equal(response.status, 404);
             return $q.when();
         });
-        return promise;
     }));
 
     it('Query for point on XID', runDigestAfter(function() {
         var q = new query.Query({name: 'eq', args: ['xid', 'voltage']});
-        var promise = Point.query({rqlQuery: q.toString()}).$promise
-        .then(function(result) {
+        return Point.query({rqlQuery: q.toString()}).$promise.then(function(result) {
             assert.isArray(result);
             assert.equal(result.length, 1);
             assert.equal(result.$total, 1);
@@ -131,13 +124,11 @@ describe('Point service', function() {
         }, function(error) {
             throw new Error(error.status + ' - ' + error.statusText + ' - ' + q.toString());
         });
-        return promise;
     }));
     
     it('Query for point on ID', runDigestAfter(function() {
         var q = new query.Query({name: 'eq', args: ['id', voltagePointId]});
-        var promise = Point.query({rqlQuery: q.toString()}).$promise
-        .then(function(result) {
+        return Point.query({rqlQuery: q.toString()}).$promise.then(function(result) {
             assert.isArray(result);
             assert.equal(result.length, 1);
             assert.equal(result.$total, 1);
@@ -151,44 +142,38 @@ describe('Point service', function() {
         }, function(error) {
             throw new Error(error.status + ' - ' + error.statusText + ' - ' + q.toString());
         });
-        return promise;
     }));
 
     it('Limit query to zero results', runDigestAfter(function() {
         var q = new query.Query()
             .eq('xid', 'voltage')
             .limit(0);
-        var promise = Point.query({rqlQuery: q.toString()}).$promise
-        .then(function(result) {
+        return Point.query({rqlQuery: q.toString()}).$promise.then(function(result) {
             assert.isArray(result);
             assert.equal(result.length, 0);
             assert.equal(result.$total, 1);
         }, function(error) {
             throw new Error(error.status + ' - ' + error.statusText + ' - ' + q.toString());
         });
-        return promise;
     }));
     
     it('Limit query to one and offset by one', runDigestAfter(function() {
         var q = new query.Query()
             .eq('xid', 'voltage')
             .limit(1, 1);
-        var promise = Point.query({rqlQuery: q.toString()}).$promise
-        .then(function(result) {
+        return Point.query({rqlQuery: q.toString()}).$promise.then(function(result) {
             assert.isArray(result);
             assert.equal(result.length, 0);
             assert.equal(result.$total, 1);
         }, function(error) {
             throw new Error(error.status + ' - ' + error.statusText + ' - ' + q.toString());
         });
-        return promise;
     }));
     
     it('Query by data source name', runDigestAfter(function() {
         var q = new query.Query()
             .eq('dataSourceName', 'Dashboard Demo');
-        var promise = Point.query({rqlQuery: q.toString()}).$promise
-        .then(function(result) {
+        return Point.query({rqlQuery: q.toString()}).$promise.then(function(result) {
             assert.isArray(result);
             assert.equal(result.length, 5);
             assert.equal(result.$total, 5);
@@ -198,14 +183,12 @@ describe('Point service', function() {
         }, function(error) {
             throw new Error(error.status + ' - ' + error.statusText + ' - ' + q.toString());
         });
-        return promise;
     }));
     
     it('Query by data source xid', runDigestAfter(function() {
         var q = new query.Query()
             .eq('dataSourceXid', 'DS_997094');
-        var promise = Point.query({rqlQuery: q.toString()}).$promise
-        .then(function(result) {
+        return Point.query({rqlQuery: q.toString()}).$promise.then(function(result) {
             assert.isArray(result);
             assert.equal(result.length, 5);
             assert.equal(result.$total, 5);
@@ -215,15 +198,13 @@ describe('Point service', function() {
         }, function(error) {
             throw new Error(error.status + ' - ' + error.statusText + ' - ' + q.toString());
         });
-        return promise;
     }));
 
     it('Query by data source name, limit and offset', runDigestAfter(function() {
         var q = new query.Query()
             .eq('dataSourceName', 'Dashboard Demo')
             .limit(3, 2);
-        var promise = Point.query({rqlQuery: q.toString()}).$promise
-        .then(function(result) {
+        return Point.query({rqlQuery: q.toString()}).$promise.then(function(result) {
             assert.isArray(result);
             assert.equal(result.length, 3);
             assert.equal(result.$total, 5);
@@ -233,15 +214,13 @@ describe('Point service', function() {
         }, function(error) {
             throw new Error(error.status + ' - ' + error.statusText + ' - ' + q.toString());
         });
-        return promise;
     }));
     
     it('Query by data source name, offset so results are limited', runDigestAfter(function() {
         var q = new query.Query()
             .eq('dataSourceName', 'Dashboard Demo')
             .limit(100, 3);
-        var promise = Point.query({rqlQuery: q.toString()}).$promise
-        .then(function(result) {
+        return Point.query({rqlQuery: q.toString()}).$promise.then(function(result) {
             assert.isArray(result);
             assert.equal(result.length, 2);
             assert.equal(result.$total, 5);
@@ -251,30 +230,26 @@ describe('Point service', function() {
         }, function(error) {
             throw new Error(error.status + ' - ' + error.statusText + ' - ' + q.toString());
         });
-        return promise;
     }));
     
     it('Query by data source name, offset so no results returned', runDigestAfter(function() {
         var q = new query.Query()
             .eq('dataSourceName', 'Dashboard Demo')
             .limit(100, 10);
-        var promise = Point.query({rqlQuery: q.toString()}).$promise
-        .then(function(result) {
+        return Point.query({rqlQuery: q.toString()}).$promise.then(function(result) {
             assert.isArray(result);
             assert.equal(result.length, 0);
             assert.equal(result.$total, 5);
         }, function(error) {
             throw new Error(error.status + ' - ' + error.statusText + ' - ' + q.toString());
         });
-        return promise;
     }));
     
     it('Query by data source XID and device name', runDigestAfter(function() {
         var q = new query.Query()
             .eq('dataSourceXid', 'vmeters')
             .eq('deviceName', 'Meter 1');
-        var promise = Point.query({rqlQuery: q.toString()}).$promise
-        .then(function(result) {
+        return Point.query({rqlQuery: q.toString()}).$promise.then(function(result) {
             assert.isArray(result);
             assert.equal(result.length, 13);
             assert.equal(result.$total, 13);
@@ -284,15 +259,13 @@ describe('Point service', function() {
         }, function(error) {
             throw new Error(error.status + ' - ' + error.statusText + ' - ' + q.toString());
         });
-        return promise;
     }));
     
     it('Query by data source XID and multiple device names using IN', runDigestAfter(function() {
         var q = new query.Query()
             .eq('dataSourceXid', 'vmeters')
             ['in']('deviceName', 'Meter 1', 'Meter 2');
-        var promise = Point.query({rqlQuery: q.toString()}).$promise
-        .then(function(result) {
+        return Point.query({rqlQuery: q.toString()}).$promise.then(function(result) {
             assert.isArray(result);
             assert.equal(result.length, 26);
             assert.equal(result.$total, 26);
@@ -302,7 +275,6 @@ describe('Point service', function() {
         }, function(error) {
             throw new Error(error.status + ' - ' + error.statusText + ' - ' + q.toString());
         });
-        return promise;
     }));
     
     it('Query by data source XID and multiple device names using OR', runDigestAfter(function() {
@@ -315,8 +287,7 @@ describe('Point service', function() {
             .eq('dataSourceXid', 'vmeters')
             .push(orPart);
         
-        var promise = Point.query({rqlQuery: q.toString()}).$promise
-        .then(function(result) {
+        return Point.query({rqlQuery: q.toString()}).$promise.then(function(result) {
             assert.isArray(result);
             assert.equal(result.length, 26);
             assert.equal(result.$total, 26);
@@ -326,7 +297,6 @@ describe('Point service', function() {
         }, function(error) {
             throw new Error(error.status + ' - ' + error.statusText + ' - ' + q.toString());
         });
-        return promise;
     }));
     
     it('Query with root OR term', runDigestAfter(function() {
@@ -340,8 +310,7 @@ describe('Point service', function() {
         
         var q = new query.Query({name: 'or', args: [queryDs1, queryDs2]});
         
-        var promise = Point.query({rqlQuery: q.toString()}).$promise
-        .then(function(result) {
+        return Point.query({rqlQuery: q.toString()}).$promise.then(function(result) {
             assert.isArray(result);
             assert.equal(result.length, 14);
             assert.equal(result.$total, 14);
@@ -351,7 +320,6 @@ describe('Point service', function() {
         }, function(error) {
             throw new Error(error.status + ' - ' + error.statusText + ' - ' + q.toString());
         });
-        return promise;
     }));
     
     it('Query with root OR term and nested OR term', runDigestAfter(function() {
@@ -370,8 +338,7 @@ describe('Point service', function() {
         
         var q = new query.Query({name: 'or', args: [queryDs1, queryDs2]});
         
-        var promise = Point.query({rqlQuery: q.toString()}).$promise
-        .then(function(result) {
+        return Point.query({rqlQuery: q.toString()}).$promise.then(function(result) {
             assert.isArray(result);
             assert.equal(result.length, 27);
             assert.equal(result.$total, 27);
@@ -381,15 +348,13 @@ describe('Point service', function() {
         }, function(error) {
             throw new Error(error.status + ' - ' + error.statusText + ' - ' + q.toString());
         });
-        return promise;
     }));
 
     it('Query for enabled points using \'Y\'', runDigestAfter(function() {
         var q = new query.Query()
             .eq('dataSourceXid', 'DS_997094')
             .eq('enabled', 'Y');
-        var promise = Point.query({rqlQuery: q.toString()}).$promise
-        .then(function(result) {
+        return Point.query({rqlQuery: q.toString()}).$promise.then(function(result) {
             assert.isArray(result);
             assert.equal(result.length, 4);
             assert.equal(result.$total, 4);
@@ -399,15 +364,13 @@ describe('Point service', function() {
         }, function(error) {
             throw new Error(error.status + ' - ' + error.statusText + ' - ' + q.toString());
         });
-        return promise;
     }));
     
     it('Query for enabled points using \'true\'', runDigestAfter(function() {
         var q = new query.Query()
             .eq('dataSourceXid', 'DS_997094')
             .eq('enabled', 'true');
-        var promise = Point.query({rqlQuery: q.toString()}).$promise
-        .then(function(result) {
+        return Point.query({rqlQuery: q.toString()}).$promise.then(function(result) {
             assert.isArray(result);
             assert.equal(result.length, 4);
             assert.equal(result.$total, 4);
@@ -417,15 +380,13 @@ describe('Point service', function() {
         }, function(error) {
             throw new Error(error.status + ' - ' + error.statusText + ' - ' + q.toString());
         });
-        return promise;
     }));
     
     it('Query for enabled points using true', runDigestAfter(function() {
         var q = new query.Query()
             .eq('dataSourceXid', 'DS_997094')
             .eq('enabled', true);
-        var promise = Point.query({rqlQuery: q.toString()}).$promise
-        .then(function(result) {
+        return Point.query({rqlQuery: q.toString()}).$promise.then(function(result) {
             assert.isArray(result);
             assert.equal(result.length, 4);
             assert.equal(result.$total, 4);
@@ -435,15 +396,13 @@ describe('Point service', function() {
         }, function(error) {
             throw new Error(error.status + ' - ' + error.statusText + ' - ' + q.toString());
         });
-        return promise;
     }));
     
     it('Query for enabled points using \'N\'', runDigestAfter(function() {
         var q = new query.Query()
             .eq('dataSourceXid', 'DS_997094')
             .eq('enabled', 'N');
-        var promise = Point.query({rqlQuery: q.toString()}).$promise
-        .then(function(result) {
+        return Point.query({rqlQuery: q.toString()}).$promise.then(function(result) {
             assert.isArray(result);
             assert.equal(result.length, 1);
             assert.equal(result.$total, 1);
@@ -453,15 +412,13 @@ describe('Point service', function() {
         }, function(error) {
             throw new Error(error.status + ' - ' + error.statusText + ' - ' + q.toString());
         });
-        return promise;
     }));
     
     it('Query for enabled points using \'false\'', runDigestAfter(function() {
         var q = new query.Query()
             .eq('dataSourceXid', 'DS_997094')
             .eq('enabled', 'false');
-        var promise = Point.query({rqlQuery: q.toString()}).$promise
-        .then(function(result) {
+        return Point.query({rqlQuery: q.toString()}).$promise.then(function(result) {
             assert.isArray(result);
             assert.equal(result.length, 1);
             assert.equal(result.$total, 1);
@@ -471,15 +428,13 @@ describe('Point service', function() {
         }, function(error) {
             throw new Error(error.status + ' - ' + error.statusText + ' - ' + q.toString());
         });
-        return promise;
     }));
     
     it('Query for enabled points using false', runDigestAfter(function() {
         var q = new query.Query()
             .eq('dataSourceXid', 'DS_997094')
             .eq('enabled', false);
-        var promise = Point.query({rqlQuery: q.toString()}).$promise
-        .then(function(result) {
+        return Point.query({rqlQuery: q.toString()}).$promise.then(function(result) {
             assert.isArray(result);
             assert.equal(result.length, 1);
             assert.equal(result.$total, 1);
@@ -489,7 +444,6 @@ describe('Point service', function() {
         }, function(error) {
             throw new Error(error.status + ' - ' + error.statusText + ' - ' + q.toString());
         });
-        return promise;
     }));
     
     it('Query for points in "Demo" folder', runDigestAfter(function() {
@@ -508,14 +462,14 @@ describe('Point service', function() {
                 throw new Error(error.status + ' - ' + error.statusText + ' - ' + q.toString());
             });
         }, function(error) {
+            if (error instanceof Error) return error;
             throw new Error(error.status + ' - ' + error.statusText + ' - Error retrieving point hierarchy folder "Demo"');
         });
     }));
 
     it('Query on non-existing property', runDigestAfter(function() {
         var q = new query.Query({name: 'eq', args: ['xyz', 'blah']});
-        var promise = Point.query({rqlQuery: q.toString()}).$promise
-        .then(function(result) {
+        return Point.query({rqlQuery: q.toString()}).$promise.then(function(result) {
             throw new Error('Returned successful result for invalid query');
         }, function(error) {
             assert.equal(error.status, 500);
@@ -524,7 +478,6 @@ describe('Point service', function() {
             assert.isString(error.data.stackTrace);
             return $q.when();
         });
-        return promise;
     }));
 
     var createdPointXid;
@@ -582,6 +535,7 @@ describe('Point service', function() {
                     throw new Error(error.status + ' - ' + error.statusText + ' - Couldn\'t get updated point');
                 });
             }, function(error) {
+                if (error instanceof Error) return error;
                 throw new Error(error.status + ' - ' + error.statusText);
             });
         }, function(error) {
