189d0c07f2289beb6ebec6022988d3de3218d3d4
Jared Wiltshire <jazdw@users.noreply.github.com>
2016-11-11 16:14:33 -0700

    Use moment for finding date periods in amcharts

--- Custom Dashboards/web/js/mango-3.3/maDashboards.js
+++ Custom Dashboards/web/js/mango-3.3/maDashboards.js
@@ -288,6 +288,49 @@ function($rootScope, mangoWatchdog, maDashboardsInsertCss, cssInjector, MA_ROLLU
     AmCharts.formatDate = function(date, format, chart) {
         return moment(date).format(format);
     };
+    
+    AmCharts._resetDateToMin = AmCharts.resetDateToMin;
+    AmCharts.resetDateToMin = function(date, period, count, firstDateOfWeek) {
+        var m = moment(date);
+        switch(period) {
+        case 'YYYY':
+            m.year(roundDownToNearestX(m.year(), count));
+            m.startOf('year');
+            break;
+        case 'MM':
+            m.month(roundDownToNearestX(m.month(), count));
+            m.startOf('month');
+            break;
+        case 'WW':
+            m.week(roundDownToNearestX(m.week(), count));
+            m.startOf('week');
+            break;
+        case 'DD':
+            //m.date(roundDownToNearestX(m.date(), count));
+            m.startOf('day');
+            break;
+        case 'hh':
+            m.hour(roundDownToNearestX(m.hour(), count));
+            m.startOf('hour');
+            break;
+        case 'mm':
+            m.minute(roundDownToNearestX(m.minute(), count));
+            m.startOf('minute');
+            break;
+        case 'ss':
+            m.second(roundDownToNearestX(m.second(), count));
+            m.startOf('second');
+            break;
+        case 'fff':
+            m.millisecond(roundDownToNearestX(m.millisecond(), count));
+            break;
+        }
+        return m.toDate();
+
+        function roundDownToNearestX(a,x) {
+            return a - a % x;
+        }
+    };
 }]);
 
 return maDashboards;
