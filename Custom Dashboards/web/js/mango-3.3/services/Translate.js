85700ba97fada18fcd0d89c348ce0668380e0ca2
Jared Wiltshire <jazdw@users.noreply.github.com>
2016-11-15 15:10:35 -0700

    Use server side language

--- Custom Dashboards/web/js/mango-3.3/services/Translate.js
+++ Custom Dashboards/web/js/mango-3.3/services/Translate.js
@@ -54,85 +54,89 @@ define(['angular', 'globalize', 'globalize/message', 'cldr/unresolved'], functio
 *
 */
 function translateFactory($http, $q) {
-	var Translate = function() {};
+    var Translate = function() {};
 
-	var likelySubtagsUrl = '/resources/cldr-data/supplemental/likelySubtags.json';
-	Translate.likelySubtags = $http.get(likelySubtagsUrl).then(function(likelySubtags) {
-		Globalize.load(likelySubtags.data);
-	});
+    var likelySubtagsUrl = '/resources/cldr-data/supplemental/likelySubtags.json';
+    Translate.likelySubtags = $http.get(likelySubtagsUrl).then(function(likelySubtags) {
+        Globalize.load(likelySubtags.data);
+    });
 
-	Translate.tr = function(key, args) {
-		if (!angular.isArray(args)) {
+    Translate.tr = function(key, args) {
+        if (!angular.isArray(args)) {
             args = Array.prototype.slice.call(arguments, 1);
         }
 
         var namespace = key.split('.')[0];
         return Translate.loadNamespaces(namespace).then(function() {
-        	try {
-        		return Translate.trSync(key, args);
-        	} catch (e) {
-        		return $.Deferred().reject(e);
-        	}
+            try {
+                return Translate.trSync(key, args);
+            } catch (e) {
+                return $.Deferred().reject(e);
+            }
         });
-	};
+    };
 
-	Translate.trSync = function(key, args) {
-		if (!angular.isArray(args)) {
+    Translate.trSync = function(key, args) {
+        if (!angular.isArray(args)) {
             args = Array.prototype.slice.call(arguments, 1);
         }
-		return Globalize.messageFormatter(key).apply(Globalize, args);
-	};
+        return Globalize.messageFormatter(key).apply(Globalize, args);
+    };
 
-	Translate.loadedNamespaces = {};
+    Translate.loadedNamespaces = {};
 
-	// TODO allow setting language, for now we just use whatever the server returns
-	Translate.loadNamespaces = function(namespaces) {
-		if (!angular.isArray(namespaces)) {
-			namespaces = Array.prototype.slice.call(arguments);
+    // TODO allow setting language, for now we just use whatever the server returns
+    Translate.loadNamespaces = function(namespaces) {
+        if (!angular.isArray(namespaces)) {
+            namespaces = Array.prototype.slice.call(arguments);
         }
 
-		return Translate.likelySubtags.then(function() {
-			var namespaceRequests = [];
-			for (var i = 0; i < namespaces.length; i++) {
-				var namespace = namespaces[i];
-				var request = Translate.loadedNamespaces[namespace];
-				if (!request) {
-					var translationsUrl = '/rest/v1/translations/';
-					if (namespace === 'public' || namespace === 'login' || namespace === 'header') {
-						translationsUrl += 'public/';
-					}
+        return Translate.likelySubtags.then(function() {
+            var namespaceRequests = [];
+            for (var i = 0; i < namespaces.length; i++) {
+                var namespace = namespaces[i];
+                var request = Translate.loadedNamespaces[namespace];
+                if (!request) {
+                    var translationsUrl = '/rest/v1/translations/';
+                    if (namespace === 'public' || namespace === 'login' || namespace === 'header') {
+                        translationsUrl += 'public/';
+                    }
 
-					request = $http.get(translationsUrl + encodeURIComponent(namespace), {
-						params: {
-							//language: 'XXX'
-						}
-					});
-					request.then(null, removeFromLoaded.bind(null, namespace));
-					Translate.loadedNamespaces[namespace] = request;
-				}
-				namespaceRequests.push(request);
-			}
-			return $q.all(namespaceRequests);
-		}).then(function(result) {
-			for (var i = 0; i < result.length; i++) {
-				var data = result[i].data;
-				if (!data.loaded) {
-					Globalize.loadMessages(data.translations);
-					data.loaded = true;
-				}
-				// use server returned language
-				if (!Globalize.locale()) {
-					Globalize.locale(data.locale);
-				}
-			}
-		});
-	};
+                    request = $http.get(translationsUrl + encodeURIComponent(namespace), {
+                        params: {
+                            //language: 'XXX',
+                            server: true
+                        }
+                    });
+                    request.then(null, removeFromLoaded.bind(null, namespace));
+                    Translate.loadedNamespaces[namespace] = request;
+                }
+                namespaceRequests.push(request);
+            }
+            return $q.all(namespaceRequests);
+        }).then(function(result) {
+            var allData = {};
+            for (var i = 0; i < result.length; i++) {
+                var data = result[i].data;
+                angular.merge(allData, data);
+                if (!data.loaded) {
+                    Globalize.loadMessages(data.translations);
+                    data.loaded = true;
+                }
+                // use server returned language
+                if (!Globalize.locale()) {
+                    Globalize.locale(data.locale);
+                }
+            }
+            return allData;
+        });
+    };
 
-	function removeFromLoaded(namespace) {
-		delete Translate.loadedNamespaces[namespace];
-	}
+    function removeFromLoaded(namespace) {
+        delete Translate.loadedNamespaces[namespace];
+    }
 
-	return Translate;
+    return Translate;
 }
 
 translateFactory.$inject = ['$http', '$q'];
