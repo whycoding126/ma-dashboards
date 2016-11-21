bf61fed099495477e5317ff3ee8c5a56a0b66725
Jared Wiltshire <jazdw@users.noreply.github.com>
2016-11-21 13:00:26 -0700

    Work around focus bug

--- Custom Dashboards/web/js/mango-3.4/directives/clearable.js
+++ Custom Dashboards/web/js/mango-3.4/directives/clearable.js
@@ -6,18 +6,25 @@
 define(['angular'], function(angular) {
 'use strict';
 
-clearable.$inject = ['$compile'];
-function clearable($compile) {
+clearable.$inject = ['$compile', '$timeout'];
+function clearable($compile, $timeout) {
     return {
         restrict: 'A',
         require: 'mdAutocomplete',
         scope: false,
         link: function ($scope, $element, $attrs, $mdAutocompleteCtrl) {
             var wrapElement = $element.find('md-autocomplete-wrap');
-            var template = '<button ng-if="(maClearable || maClearable==undefined) && $mdAutocompleteCtrl.scope.searchText && !$mdAutocompleteCtrl.isDisabled" ng-click="$mdAutocompleteCtrl.clear($event)" aria-label="{{\'dashboards.v3.app.clear\' | tr}}"><md-icon>clear</md-icon></button>';
+            var template = '<button ng-if="(maClearable || maClearable==undefined) && $mdAutocompleteCtrl.scope.searchText && !$mdAutocompleteCtrl.isDisabled" ng-click="clear()" aria-label="{{\'dashboards.v3.app.clear\' | tr}}"><md-icon>clear</md-icon></button>';
             
             var buttonScope = $scope.$new(false);
             buttonScope.$mdAutocompleteCtrl = $mdAutocompleteCtrl;
+            buttonScope.clear = function() {
+                $element.find('input').focus();
+                $timeout(function() {
+                    $mdAutocompleteCtrl.scope.searchText = '';
+                },0)
+            }
+            
             var buttonElement = $compile(template)(buttonScope);
             wrapElement.append(buttonElement);
         }
