946478e1a1d9520ec7a0b28cb8b00c0e07d04dd6
Will Geller <will.geller@gmail.com>
2016-12-12 16:28:42 -0700

    Add google’s marker icons to image folder; Add dark theme option to info window; add support for toggling infoWindows from marker

--- Custom Dashboards/web/js/mango-3.4/components/maMap/maMap.js
+++ Custom Dashboards/web/js/mango-3.4/components/maMap/maMap.js
@@ -3,25 +3,48 @@
  * @author Will Geller
  */
 
-define(['angular', 'require'], function(angular, require) {
-'use strict';
+define(['angular', 'require'], function (angular, require) {
+    'use strict';
+    maMapController.$inject = ['$timeout', 'NgMap'];
+    function maMapController($timeout, NgMap) {
+        this.render = false;
+        this.infoWindowCache = {};
 
-function maMapController() {
-    
-    this.$onChanges = function(changes) {
-        // console.log(changes);
+        $timeout(function () {
+            this.render = true;
+        }.bind(this), 100);
+
+        NgMap.getMap().then(function (map) {
+            this.map = map;
+        }.bind(this));
+
+        this.toggleInfoWindow = function(e, windowId, markerId) {
+            // console.log(e, windowId, markerId);
+            if (!this.infoWindowCache[windowId]) {
+                this.map.showInfoWindow(windowId, markerId);
+                this.infoWindowCache[windowId] = true;
+            }
+            else {
+                this.map.hideInfoWindow(windowId);
+                this.infoWindowCache[windowId] = false;
+            }
+        }.bind(this);
+
+        this.$onChanges = function (changes) {
+            // console.log(changes);
+        };
+    }
+
+    return {
+        bindings: {
+            zoom: '<',
+            center: '@',
+            mapType: '@',
+            infoWindowTheme: '@',
+            height: '@'
+        },
+        controller: maMapController,
+        templateUrl: require.toUrl('./maMap.html'),
+        transclude: true
     };
-    
-}
-
-return {
-    bindings: {
-        zoom: '<',
-        center: '@'
-    },
-    controller: maMapController,
-    templateUrl: require.toUrl('./maMap.html'),
-    transclude: true
-};
-
 }); // define
