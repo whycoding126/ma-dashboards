ea995988612aa7239e435d1aae76765e859c693c
terrypacker <username@users.noreply.github.com>
2017-01-06 13:59:01 -1000

    Changes for system settings use in REST.

--- Custom Dashboards/src/com/infiniteautomation/dashboards/DashboardsSystemSettingsDefinition.java
+++ Custom Dashboards/src/com/infiniteautomation/dashboards/DashboardsSystemSettingsDefinition.java
@@ -4,6 +4,10 @@
  */
 package com.infiniteautomation.dashboards;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.serotonin.m2m2.i18n.ProcessResult;
 import com.serotonin.m2m2.module.SystemSettingsDefinition;
 
 /**
@@ -26,6 +30,51 @@ public class DashboardsSystemSettingsDefinition extends SystemSettingsDefinition
 	@Override
 	public String getSectionJspPath() {
 		return "web/settings.jsp";
+	}
+
+	/* (non-Javadoc)
+	 * @see com.serotonin.m2m2.module.SystemSettingsDefinition#getDefaultValues()
+	 */
+	@Override
+	public Map<String, Object> getDefaultValues() {
+		Map<String, Object> defaults = new HashMap<String, Object>();
+		defaults.put(DashboardsCommon.DASHBOARDS_FIRST_LOGIN_PAGE, DashboardsCommon.DEFAULT_DASHBOARDS_FIRST_LOGIN_PAGE);
+		defaults.put(DashboardsCommon.DASHBOARDS_FIRST_USER_LOGIN_PAGE, DashboardsCommon.DEFAULT_DASHBOARDS_FIRST_USER_LOGIN_PAGE);
+		defaults.put(DashboardsCommon.DASHBOARDS_ICON_DESTINATION, DashboardsCommon.DEFAULT_DASHBOARDS_ICON_DESTINATION);
+		defaults.put(DashboardsCommon.DASHBOARDS_ICON_LOCATION, DashboardsCommon.DEFAULT_DASHBOARDS_ICON_LOCATION);
+		defaults.put(DashboardsCommon.DASHBOARDS_LOGGED_IN_PAGE, DashboardsCommon.DEFAULT_DASHBOARDS_LOGGED_IN_PAGE);
+		defaults.put(DashboardsCommon.DASHBOARDS_LOGIN_PAGE, DashboardsCommon.DEFAULT_DASHBOARDS_LOGIN_PAGE);
+		defaults.put(DashboardsCommon.DASHBOARDS_PRIVATE_FILES_LOCATION, DashboardsCommon.DEFAULT_DASHBOARDS_PRIVATE_FILES_LOCATION);
+		defaults.put(DashboardsCommon.DASHBOARDS_PRIVATE_URL_PREFIX, DashboardsCommon.DEFAULT_DASHBOARDS_PRIVATE_URL_PREFIX);
+		defaults.put(DashboardsCommon.DASHBOARDS_PUBLIC_FILES_LOCATION, DashboardsCommon.DEFAULT_DASHBOARDS_PUBLIC_FILES_LOCATION);
+		defaults.put(DashboardsCommon.DASHBOARDS_PUBLIC_URL_PREFIX, DashboardsCommon.DEFAULT_DASHBOARDS_PUBLIC_URL_PREFIX);
+		defaults.put(DashboardsCommon.DASHBOARDS_UNAUTHORIZED_PAGE, DashboardsCommon.DEFAULT_DASHBOARDS_UNAUTHORIZED_PAGE);
+			
+		return defaults;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.serotonin.m2m2.module.SystemSettingsDefinition#convertToValueFromCode(java.lang.String, java.lang.String)
+	 */
+	@Override
+	public Integer convertToValueFromCode(String key, String code) {
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.serotonin.m2m2.module.SystemSettingsDefinition#convertToCodeFromValue(java.lang.String, java.lang.Integer)
+	 */
+	@Override
+	public String convertToCodeFromValue(String key, Integer value) {
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.serotonin.m2m2.module.SystemSettingsDefinition#validateSettings(java.util.Map, com.serotonin.m2m2.i18n.ProcessResult)
+	 */
+	@Override
+	public void validateSettings(Map<String, Object> settings, ProcessResult response) {
+		// TODO Not Sure How to Validate These?
 	}
 
 }
