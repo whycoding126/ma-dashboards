278813fdf6da6b4d92a757cfeaa93dd841fdc56a
Will Geller <will.geller@gmail.com>
2017-01-13 17:45:38 -0700

    watch for $onChanges to page xid and update markup

--- Custom Dashboards/web/mdAdmin/components/pageView/pageView.js
+++ Custom Dashboards/web/mdAdmin/components/pageView/pageView.js
@@ -7,19 +7,21 @@ define(['angular', 'require'], function(angular, require) {
 'use strict';
 
 var pageViewController = function pageViewController($scope, Page, jsonStoreEventManager, User) {
-    var $ctrl = this;
     var SUBSCRIPTION_TYPES = ['add', 'update'];
-
+    
+    var $ctrl = this;
     $ctrl.User = User;
 
-    Page.loadPage($ctrl.xid).then(function(page) {
-        $ctrl.page = page;
-        $ctrl.markup = page.jsonData.markup;
-    });
+    $ctrl.$onChanges = function(changes) {
+        Page.loadPage($ctrl.xid).then(function(page) {
+            $ctrl.page = page;
+            $ctrl.markup = page.jsonData.markup;
+        });
 
-    jsonStoreEventManager.smartSubscribe($scope, $ctrl.xid, SUBSCRIPTION_TYPES, function updateHandler(event, payload) {
-        $ctrl.markup = payload.object.jsonData.markup;
-    });
+        jsonStoreEventManager.smartSubscribe($scope, $ctrl.xid, SUBSCRIPTION_TYPES, function updateHandler(event, payload) {
+            $ctrl.markup = payload.object.jsonData.markup;
+        });
+    };
 };
 
 pageViewController.$inject = ['$scope', 'Page', 'jsonStoreEventManager', 'User'];
