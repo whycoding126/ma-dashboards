461d44625273a1a5f476642cdde1bfc56bbbc3fd
Jared Wiltshire <jazdw@users.noreply.github.com>
2017-01-16 12:07:32 -0700

    Remove debounce from live preview and convert to controller

--- Custom Dashboards/web/mdAdmin/directives/liveEditor/livePreview.js
+++ Custom Dashboards/web/mdAdmin/directives/liveEditor/livePreview.js
@@ -6,50 +6,47 @@
 define(['require'], function(require) {
 'use strict';
 
-var livePreview = function($compile, $timeout) {
-    return {
-        scope: false,
-        link: function($scope, $element, $attrs) {
-            var childScope = $scope.$new();
-            var timeoutPromise;
-            
-            var debounceTimeout = 1000;
-            if ($attrs.debounce) {
-                debounceTimeout = parseInt($attrs.debounce, 10);
-            }
-            
-            $scope.$watch($attrs.livePreview, function(newValue, oldValue) {
-                if (newValue === oldValue || !oldValue || debounceTimeout === 0) {
-                    updatePreview(newValue);
-                } else {
-                    if (timeoutPromise) {
-                        $timeout.cancel(timeoutPromise);
-                        timeoutPromise = null;
-                    }
-                    timeoutPromise = $timeout(updatePreview, debounceTimeout, true, newValue);
-                }
-            });
-            
-            function updatePreview(text) {
-                timeoutPromise = null;
-                
-                childScope.$destroy();
-                childScope = $scope.$new();
-                
-                if (text) {
-                    var compileText = '<div>' + text + '</div>';
-                    var $div = $compile(compileText)(childScope);
-                    $element.html($div.contents());
-                } else {
-                    $element.empty();
-                }
-            }
-        }
-    };
+LivePreviewController.$inject = ['$scope', '$compile', '$element'];
+function LivePreviewController($scope, $compile, $element) {
+    this.$scope = $scope;
+    this.$compile = $compile;
+    this.$element = $element;
+    this.childScope = null;
+}
+
+LivePreviewController.prototype.$onChanges = function(changes) {
+    if (changes.livePreview) {
+        this.updatePreview();
+    }
 };
 
-livePreview.$inject = ['$compile', '$timeout'];
+LivePreviewController.prototype.updatePreview = function updatePreview() {
+    if (this.childScope) {
+        this.childScope.$destroy();
+        this.childScope = null;
+    }
+    
+    if (this.livePreview) {
+        this.childScope = this.$scope.$new();
+        var compileText = '<div>' + this.livePreview + '</div>';
+        var $div = this.$compile(compileText)(this.childScope);
+        this.$element.html($div.contents());
+    } else {
+        this.$element.empty();
+    }
+};
 
-return livePreview;
+LivePreview.$inject = [];
+function LivePreview() {
+    return {
+        scope: false,
+        bindToController: {
+            livePreview: '<'
+        },
+        controller: LivePreviewController
+    };
+}
+
+return LivePreview;
 
 }); // define
