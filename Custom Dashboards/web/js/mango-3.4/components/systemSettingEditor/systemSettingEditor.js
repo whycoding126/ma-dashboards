7946ab7cad810bf47fb2ec28517f0b33a407059d
Jared Wiltshire <jazdw@users.noreply.github.com>
2017-01-18 15:26:01 -0700

    Add save/error feedback to system setting editors

--- Custom Dashboards/web/js/mango-3.4/components/systemSettingEditor/systemSettingEditor.js
+++ Custom Dashboards/web/js/mango-3.4/components/systemSettingEditor/systemSettingEditor.js
@@ -6,9 +6,13 @@
 define(['angular', 'require'], function(angular, require) {
 'use strict';
 
-SystemSettingEditorController.$inject = ['SystemSettings'];
-function SystemSettingEditorController(SystemSettings) {
+SystemSettingEditorController.$inject = ['SystemSettings', '$timeout', '$q'];
+function SystemSettingEditorController(SystemSettings, $timeout, $q) {
     this.SystemSettings = SystemSettings;
+    this.$timeout = $timeout;
+    this.$q = $q;
+    
+    this.messages = {};
 }
 
 SystemSettingEditorController.prototype.$onChanges = function(changes) {
@@ -16,6 +20,34 @@ SystemSettingEditorController.prototype.$onChanges = function(changes) {
         this.systemSetting = new this.SystemSettings(this.key);
         this.systemSetting.getValue();
     }
+};
+
+SystemSettingEditorController.prototype.settingChanged = function settingChanged() {
+    var $ctrl = this;
+    this.done = false;
+    this.error = false;
+    delete $ctrl.messages.errorSaving;
+    
+    // dont show the sync icon for saves of less than 200ms, stops icon flashing
+    var delay = this.$timeout(function() {
+        this.saving = true;
+    }, 200);
+    
+    this.systemSetting.setValue().then(function() {
+        $ctrl.$timeout.cancel(delay);
+        $ctrl.saving = false;
+        $ctrl.done = true;
+    }, function() {
+        $ctrl.$timeout.cancel(delay);
+        $ctrl.saving = false;
+        $ctrl.error = true;
+        $ctrl.messages.errorSaving = true;
+        return $ctrl.$q.reject();
+    }).then(function() {
+        return $ctrl.$timeout(angular.noop, 5000);
+    }).then(function() {
+        $ctrl.done = false;
+    });
 };
 
 return {
