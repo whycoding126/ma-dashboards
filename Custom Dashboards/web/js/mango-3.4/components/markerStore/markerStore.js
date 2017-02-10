115c16021397797843b1f1e311691d232ae6b512
Will Geller <will.geller@gmail.com>
2017-02-10 14:28:03 -0700

    Add ability to set marker icon

--- Custom Dashboards/web/js/mango-3.4/components/markerStore/markerStore.js
+++ Custom Dashboards/web/js/mango-3.4/components/markerStore/markerStore.js
@@ -7,10 +7,73 @@ define(['angular', 'require'], function(angular, require) {
     'use strict';
 
   
-    // markerStoreController.$inject = [];
+    markerStoreController.$inject = [];
 
     function markerStoreController() {
         var $ctrl = this;
+
+        $ctrl.markerIcons = [
+            {
+                name: 'Red Dot',
+                image: 'img/map-markers/red-dot.png'
+            },
+            {
+                name: 'Red Small',
+                image: 'img/map-markers/mm_20_red.png'
+            },
+            {
+                name: 'Blue Dot',
+                image: 'img/map-markers/blue-dot.png'
+            },
+            {
+                name: 'Blue Small',
+                image: 'img/map-markers/mm_20_blue.png'
+            },
+            {
+                name: 'Green Dot',
+                image: 'img/map-markers/green-dot.png'
+            },
+            {
+                name: 'Green Small',
+                image: 'img/map-markers/mm_20_green.png'
+            },
+            {
+                name: 'Yellow Dot',
+                image: 'img/map-markers/yellow-dot.png'
+            },
+            {
+                name: 'Yellow Small',
+                image: 'img/map-markers/mm_20_yellow.png'
+            },
+            {
+                name: 'Purple Dot',
+                image: 'img/map-markers/purple-dot.png'
+            },
+            {
+                name: 'Purple Small',
+                image: 'img/map-markers/mm_20_purple.png'
+            },
+            {
+                name: 'Orange Dot',
+                image: 'img/map-markers/orange-dot.png'
+            },
+            {
+                name: 'Orange Small',
+                image: 'img/map-markers/mm_20_orange.png'
+            },
+            {
+                name: 'Brown Small',
+                image: 'img/map-markers/mm_20_brown.png'
+            },
+            {
+                name: 'Gray Small',
+                image: 'img/map-markers/mm_20_gray.png'
+            },
+            {
+                name: 'White Small',
+                image: 'img/map-markers/mm_20_white.png'
+            }
+        ];
 
         $ctrl.deleteMarker = function() {
             $ctrl.markerList.markers = $ctrl.markerList.markers.filter(function(marker) {
@@ -23,6 +86,8 @@ define(['angular', 'require'], function(angular, require) {
         $ctrl.addMarker = function() {
             $ctrl.markerList.markers.push({});
             $ctrl.selectedMarker = $ctrl.markerList.markers[$ctrl.markerList.markers.length-1];
+
+
         };
     }
 
